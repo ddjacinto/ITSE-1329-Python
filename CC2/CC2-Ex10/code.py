dad60fc8fe87e148b8832b1768eadfc7e6c3b543
ddjacinto <ddjacinto@mymail.tstc.edu>
2019-10-04 14:18:49 +0000

    completed cc2 ex10

---
+++ CC2/CC2-Ex10/code.py
@@ -0,0 +1,21 @@
+score = input("Enter score: ")
+try:
+    fscore = float(score)
+    if fscore > 1.0 :
+        print("Bad Score")
+    elif fscore < 0.0 :
+        print("Bad Score")
+    elif fscore >= .9 :
+        print('A')
+    elif fscore >= .8 :
+        print('B')
+    elif fscore >= .7 :
+        print("C")
+    elif fscore >= .6 :
+        print("D")
+    elif fscore < .6 :
+        print('F')
+except:
+    if score != int:
+        print('Please enter numeric input')
+        quit()
