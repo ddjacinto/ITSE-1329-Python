de026e011a549e9a4b6fc87a073accda3632da27
ddjacinto <ddjacinto@mymail.tstc.edu>
2019-10-05 00:18:32 +0000

    completed cc2 ex12

---
+++ CC2/CC2-Ex12/code.py
@@ -0,0 +1,8 @@
+command = input("What is the command? ")
+while command != "sit":
+    command = input("What is the command? ")
+    if command == "sit":
+        break
+    
+
+print("*puppy dog sits*")    
