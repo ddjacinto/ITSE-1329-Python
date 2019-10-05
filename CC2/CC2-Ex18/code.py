9b0b480cf77ad4db26ef2a1a222873717b688102
ddjacinto <ddjacinto@mymail.tstc.edu>
2019-10-05 16:19:44 +0000

    completed cc2 ex18

--- CC2/CC2-Ex18/code.py
+++ CC2/CC2-Ex18/code.py
@@ -3,5 +3,6 @@ rain = [.25, .76, 1.2, 1.1, .03, .97, 2.3]
 rain_total = 0.0
 
 # Your code here
-
+for thing in rain :
+    rain_total = rain_total + thing
 print('The total amount of rain was:', rain_total)
